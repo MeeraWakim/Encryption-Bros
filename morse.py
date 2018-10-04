c8d4e46c9769caf2150d10a66b9121a24277321c
MeeraWakim <meera.wakim925@gmail.com>
2018-10-04 11:18:07 -0500

    added initial framework

---
+++ morse.py
@@ -0,0 +1,69 @@
+
+def readFile (filename):
+
+	# filename passed as a string from main
+	# open file for reading
+	# readfile into script
+	x = 1
+
+	return(text)
+
+
+def encrypt (text, encryptionMethod):
+
+	# text read in from file passed in
+	# int passed in that specifies which encryption method
+
+	if (encryptionMethod == 1):
+		# do morse code encryption
+		# use a dictionary to map letters to dots/dashes?
+		x = 1
+
+	elif (encryptionMethod == 2):
+		# do other method encryption
+		x = 1
+
+	return(encryptedText)
+
+def decrypt (text, decryptionMethod):
+
+	# text read in from file passed in
+	# int passed in that specifies which decryption method
+
+	if (decryptionMethod == 1):
+		# do morse code decryption
+		# use a dictionary to map letters to dots/dashes?
+		x = 1
+
+	elif (decryptionMethod == 2):
+		# do other method decryption
+		x = 1
+
+	return(decryptedText)
+
+def writeFile (Text):
+
+	# encrypted or decrypted text passed in
+	# open a NEW file for writing
+	# write text to new file
+	x = 1
+
+def main():
+	
+	encryptDecrypt = int(input("Would you like to encrypt or decrypt a file?\n(1) for encrypt, (2) for decrypt: "))
+
+	if (encryptDecrypt == 1):
+		filename = input("Enter the name of the file you'd like to encrypt: ")
+		encryptionMethod = int(input("Which encyrption method would you like to use?\n(1) for morse code, (2) for other: "))
+		text = readFile(filename)
+		encryptedText = encrypt(text)
+		writeFile(encryptedText)
+	elif (encryptDecrypt == 2):
+		filename = input("Enter the name of the file you'd like to decrypt: ")
+		decryptionMethod = int(input("Which decyrption method would you like to use?\n(1) for morse code, (2) for other: "))
+		text = readFile(filename)
+		decryptedText = decrypt(text)
+		writeFile(decryptedText)
+
+main()
+
