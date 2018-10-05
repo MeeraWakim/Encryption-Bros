7d98f6943f1c30994b4be1c2424771af90d6634b
lihaoyang411 <haoyangli@utexas.edu>
2018-10-05 17:13:42 -0500

    finished with garrett's part

--- unit_test.py
+++ unit_test.py
@@ -6,8 +6,8 @@ class TestStringMethods(unittest.TestCase):
     def test_encryption(self):
         self.assertEqual(morse.encrypt('AaBCdem', 1), '.- .- -... -.-. -.. . --')
 
-    #def test_decryption(self):
-       # self.assertEqual(['.-','.-','-...','-.-.','-..','.','--'], ['A','A','B','C','D','E','M'])
+    def test_decryption(self):
+        self.assertEqual(morse.decrypt('.- .- -... -.-. -.. . --',1), 'AABCDEM')
 
 
 if __name__ == '__main__':
