3e5989b5887f73f7ef1ac6bdc35d857782bbc910
lihaoyang411 <haoyangli@utexas.edu>
2018-10-07 20:45:40 -0500

    change string to list

--- unit_test.py
+++ unit_test.py
@@ -6,10 +6,10 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(morse_functions.readFile('text.txt', ), ['This', 'is', 'a', 'text', 'file\nI', 'hope', 'this', 'works'])
 
     def test_encryption(self):
-        self.assertEqual(morse_functions.encrypt('AaBCdem', 1), '.- .- -... -.-. -.. . --')
+        self.assertEqual(morse_functions.encrypt(['A','a','B','C','d','e','m'], 1), '.- .- -... -.-. -.. . --')
 
     def test_decryption(self):
-        self.assertEqual(morse_functions.decrypt('.- .- -... -.-. -.. . --',1), 'AABCDEM')
+        self.assertEqual(morse_functions.decrypt(['.-','.-','-...','-.-.','-..','.','--'],1), 'AABCDEM')
 
 
 if __name__ == '__main__':
