d56ff25cea8fb6af502380fbb6c49c05b350c5a9
Ella Garcia <patriciagabriellegarcia@utexas.edu>
2018-10-14 21:44:01 -0500

    unit test

--- unittest.py
+++ unittest.py
@@ -1,13 +1,19 @@
 import unittest
-import morse
+import piglatin
 
 class TestStringMethods(unittest.TestCase):
 
     def test_encryption(self):
-        self.assertEqual(morse.encrypt('Texas Fight', 2), 'exasTay ightFay')
+        self.assertEqual(piglatin.encrypt('ACL was great and ODESZA was amazing', 1), '')
 
     def test_decryption(self):
-        self.assertEqual(morse.decrypt('OUway ucksSay',2), 'OU Sucks')
+        self.assertEqual(piglatin.decrpyt('Next is Astroworld Fest', 1),'')
+
+    def test_encryption(self):
+        self.assertEqual(piglatin.encrypt('Texas Fight', 2), 'exasTay ightFay')
+
+    def test_decryption(self):
+        self.assertEqual(piglatin.decrypt('OUway ucksSay',2), 'OU Sucks')
 
 
 if __name__ == '__main__':
